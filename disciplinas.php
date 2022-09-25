9173af49006285df21d09d57a97dc6870e92a65b
JohnPCG <jvcallera@gmail.com>
2022-09-25 19:06:31 -0300

    Inseri a table referente as Disciplinas, porém falta fazer o FOR

--- disciplinas.php
+++ disciplinas.php
@@ -23,4 +23,29 @@ include "./cabecalho.php";
     </nav>
 </div>
 
+<br>
+
+<div>
+    <table class="table table-striped table-hover">
+        <thead>  
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Sigla</th>
+                <th scope="col">Apelido</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Administração Geral</td>
+                <td></td>
+                <td>ADM GERAL</td>
+                <td><button type="button" class="btn btn-warning">Editar</button>&nbsp&nbsp&nbsp<button type="button" class="btn btn-danger">Excluir</button></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
 <?php include "./rodape.php"; ?>
