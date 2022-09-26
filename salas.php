8326f83ea75c496d9589f98b2ce7d4775b8bf99b
JohnPCG <jvcallera@gmail.com>
2022-09-26 00:47:36 -0300

    Finalização do array com for para salas

--- salas.php
+++ salas.php
@@ -36,16 +36,24 @@ include "./cabecalho.php";
                 <th scope="col"></th>
             </tr>
         </thead>
+        <?php
+
+        $salamaterias = array('Sala de Administração', 'Sala de Contabilidade', 'Sala de Linguagem de Programação', 'Sala de Engenharia de software', 'Sala de Inglês I', 'Sala de Inglês II', 'Sala de Programação de Scripts', 'Sala de Programação Web', 'Sala de Matemática Discreta', 'Sala de Geometria');
+        $apelidossalas = array('SADM', 'SCONT', 'SLPROG', 'SENGSOFTW', 'SING I', 'SING II', 'SPROGSCRIPT', 'SPROGWEB', 'SMATDISC', 'SGEO');
+
+            for($id = 0; $id < 10; $id++) {
+        ?>        
         <tbody>
             <tr>
-                <th scope="row">1</th>
-                <td>Sala de Administração Geral</td>
+                <th scope="row"><?php echo $id ?></th>
+                <td><?php echo $salamaterias[$id]?></td>
                 <td></td>
-                <td>Sala de ADM GERAL</td>
+                <td><?php echo $apelidossalas[$id]?></td>
                 <td><button type="button" class="btn btn-warning">Editar</button>&nbsp&nbsp&nbsp<button type="button" class="btn btn-danger">Excluir</button></td>
             </tr>
         </tbody>
-    </table>
+        <?php } ?>
+        </table>
 </div>
 
 <?php include "./rodape.php"; ?>
